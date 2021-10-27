9f13410cf42c3cc136fc0fede48548dd8304f33c
Szakalu <szakal407@gmail.com>
2021-10-27 11:58:11 +0200

    Initial commit

---
+++ src/main/java/pl/library/login/LoginController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.library.login;
+
+/**
+ *
+ * @author Jacek
+ */
+public class LoginController {
+    
+    private LoginView view;
+
+    public LoginController(LoginView view) {
+        this.view = view;
+    }
+    
+    
+}
